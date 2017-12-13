ba18b4508ed2059035f1391d0275b7f1394fdeab
jack <jack.nicol95@gmail.com>
2017-12-13 01:31:11 +0000

    added inverse document frequency calculation

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from vector_Space_Model import vector_space_model
 
 if __name__ == '__main__':
     vector_space_model = vector_space_model()
-    print(vector_space_model.documents[1].term_frequency)
+    print(vector_space_model.inverse_document_frequency)
--- vector_Space_Model.py
+++ vector_Space_Model.py
@@ -1,4 +1,5 @@
 import os
+import math
 from document import document
 
 
@@ -9,17 +10,20 @@ class vector_space_model(object):
         self.document_collection = os.listdir("documents/")
         self.N_documents = len(os.listdir("documents/"))
         self.documents = []
+        self.inverse_document_frequency = {}
         vector_space_model.create_documents(self)
         vector_space_model.tokenize_collection(self)
         vector_space_model.term_frequency_collection(self)
+        vector_space_model.inverse_document_frequency(self)
 
     def create_documents(self):
         document_collection = self.document_collection
         documents = self.documents
         for doc in document_collection:
             f = open("documents/" + doc)
-            f = f.read()
-            documents.append(document(doc, f))
+            text = f.read()
+            f.close()
+            documents.append(document(doc, text))
             self.documents = documents
 
     def tokenize_collection(self):
@@ -35,3 +39,14 @@ class vector_space_model(object):
     def inverse_document_frequency(self):
         N_documents = self.N_documents
         documents = self.documents
+        inverse_document_frequency = self.inverse_document_frequency
+        word_count = 0
+        for doc in documents:
+            for word in doc.tokens:
+                for d in documents:
+                    if word in d.tokens:
+                        word_count += 1
+                x = float(math.log2(float(N_documents) / float(word_count)))
+                print(x)
+                self.inverse_document_frequency[word] = x
+                word_count = 0
