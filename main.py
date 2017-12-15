1b4e97187ca7203482f5c6e699b9b351fa381218
jack <jack.nicol95@gmail.com>
2017-12-15 00:12:17 +0000

    Makes use of sys args rather than useless interface

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 from vector_Space_Model import vector_space_model
 from document import document
+import os
+import sys
+import webbrowser
+
 
 if __name__ == '__main__':
     # Create the vector_space_model class
     vector_space_model = vector_space_model()
-    while (True):
-        option = input("\nWhat would you like to do?\n\n1:Build a search query\n2:Quit\n\n")
-        if option == 1:
-            search_terms = raw_input("Enter your search query")
-            # Build the query as a document
-            query_document = document("query", search_terms)
-            # Add that document to the list of documents
-            vector_space_model.documents.append(query_document)
-            vector_space_model.build_documents()
-            vector_space_model.document_length()
-            vector_space_model.cosine_similarity()
-            try:
-                print("\n" + vector_space_model.document_return.text)
-            except AttributeError:
-                print("No match!")
-
-        elif option == 2:
-            break
-        else:
-            continue
+    # Build the query as a document
+    query_document = document("query", sys.argv[1])
+    # Add that document to the list of documents
+    vector_space_model.documents.append(query_document)
+    vector_space_model.build_documents()
+    vector_space_model.document_length()
+    vector_space_model.cosine_similarity()
+    try:
+        webbrowser.open_new_tab("documents/" + vector_space_model.document_return.name)
+    except AttributeError:
+        print("No match!")
